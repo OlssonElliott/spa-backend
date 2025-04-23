7a677de3425c4308ce1c947a20fc6457e9b901a2
Elliott <sendtoelle@hotmail.com>
2025-04-23 11:54:46 +0200

    ffs

--- spa-backend/src/main/java/com/spa_backend/spa_backend/SpaBackendApplication.java
+++ spa-backend/src/main/java/com/spa_backend/spa_backend/SpaBackendApplication.java
@@ -3,6 +3,9 @@ package com.spa_backend.spa_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class SpaBackendApplication {
 
